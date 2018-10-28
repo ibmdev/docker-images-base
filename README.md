f71616c3f64656b4496834c93e724e4da350edb8
Karim ZOUBA <karimzouba@gmail.com>
2018-10-28 22:18:58 +0100

    Initial commit image de base Docker Spring Boot

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # docker-images-base
 Liste des images de base Docker Swarm pour l'organisation
+
+## Plugin Gradle for Docker
+
+https://bmuschko.github.io/gradle-docker-plugin/
+
+https://github.com/avast/gradle-docker-compose-plugin
+
+
+
+
---
+++ src/main/java/fr/sma/hello/Data.java
@@ -0,0 +1,18 @@
+package fr.sma.hello;
+
+/**
+ * Created by a19160 on 15/10/2018.
+ */
+public class Data {
+
+
+    private String nameApplication;
+
+    public String getNameApplication() {
+        return nameApplication;
+    }
+
+    public void setNameApplication(String nameApplication) {
+        this.nameApplication = nameApplication;
+    }
+}
